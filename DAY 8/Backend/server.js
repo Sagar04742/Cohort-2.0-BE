acbd9405538b88e2448a7fcb2644f43344a7f5bb
Sagar04742 <guptasagar04742@gmail.com>
2026-04-15 10:08:27 +0530

    Initialize backend with Express, Mongoose, and dotenv; set up basic note API with CRUD operations

---
+++ DAY 8/Backend/server.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+const app = require('./src/app')
+const connectTODb = require('./src/config/database')
+
+
+
+connectTODb()
+app.listen(3000,()=>{
+    console.log('Server is running at PORT 3000')
+})
---
+++ DAY 8/Backend/src/config/database.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+function connectTODb(){
+    mongoose.connect(process.env.MONGO_URI)
+    .then(()=>{
+        console.log('Connected to database')
+    })
+}
+
+module.exports = connectTODb
