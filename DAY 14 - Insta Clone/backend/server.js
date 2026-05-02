154d783df819e91971077da22c5307c2d9a31d39
Sagar04742 <guptasagar04742@gmail.com>
2026-05-02 23:40:30 +0530

    feat: configure DNS servers for improved network resolution

--- DAY 14 - Insta Clone/backend/server.js
+++ DAY 14 - Insta Clone/backend/server.js
@@ -1,6 +1,13 @@
 require('dotenv').config()
 const app = require('./src/app')
 const connectToDatabase = require('./src/config/database')
+const dns = require('dns')
+
+dns.setServers([
+    '1.1.1.1',
+    '8.8.8.8'
+])
+
 
 const PORT = 3000
 
