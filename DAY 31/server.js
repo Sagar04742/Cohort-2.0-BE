513c4d049a5bc05b13c44909ca6c2a07cff97fc3
Sagar04742 <guptasagar04742@gmail.com>
2026-06-03 22:32:40 +0530

    Add message event handling for socket connections

--- DAY 31/server.js
+++ DAY 31/server.js
@@ -7,7 +7,17 @@ const io = new Server(httpServer,{/* options */})
 
 io.on('connection', (socket) => {
     console.log('New connection is created');
+
+    socket.on("message",(msg)=>{
+        console.log("User fired a message event");
+        console.log(msg)
+        io.emit("abc")
+    })
 });
+
+//socket.emit()
+//socket.broadcast.emit()
+//io.emit()
 
 httpServer.listen(3000, () => {
     console.log("Server is running at port 3000");
