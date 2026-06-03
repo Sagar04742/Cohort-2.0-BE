b9742251623ee8dcb609e562260a1ba8c16ac1a2
Sagar04742 <guptasagar04742@gmail.com>
2026-06-03 21:36:52 +0530

    Integrate Socket.IO for real-time communication and update package dependencies

--- DAY 31/server.js
+++ DAY 31/server.js
@@ -1,5 +1,14 @@
 import app from './src/app.js'
+import {createServer } from 'http'
+import { Server } from 'socket.io'
 
-app.listen(3000,()=>{
-    console.log("Server is running at port 3000")
-})
+const httpServer = createServer(app)
+const io = new Server(httpServer,{/* options */})
+
+io.on('connection', (socket) => {
+    console.log('New connection is created');
+});
+
+httpServer.listen(3000, () => {
+    console.log("Server is running at port 3000");
+});
