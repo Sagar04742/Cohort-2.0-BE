56e6c3356354c912e2a46bf47337fb9526a15251
Sagar04742 <guptasagar04742@gmail.com>
2026-07-11 01:22:51 +0530

    add zod for schema validation and enhance graph service functionality

--- AI BATTLE ARENA/src/services/graph.ai.service.ts
+++ AI BATTLE ARENA/src/services/graph.ai.service.ts
@@ -1,31 +1,53 @@
+import { HumanMessage } from "@langchain/core/messages";
 import {
   StateGraph,
   StateSchema,
   MessagesValue,
+  ReducedValue,
   START,
   END,
+  type GraphNode,
 } from "@langchain/langgraph";
+import { z } from "zod";
 
-type JUDGEMENT = {
-  winner: "solution_1" | "solution_2";
-  solution_1_score: number;
-  solution_2_score: number;
-};
-
-type AIBATTLESTATE = {
-  messages: typeof MessagesValue;
-  solution_1: string;
-  solution_2: string;
-  judgement: JUDGEMENT;
-};
-
-const state: AIBATTLESTATE = {
+const State = new StateSchema({
   messages: MessagesValue,
-  solution_1: "",
-  solution_2: "",
-  judgement: {
-    winner: "solution_1",
-    solution_1_score: 0,
-    solution_2_score: 0,
-  },
+  solution_1: new ReducedValue(z.string().default(""), {
+    reducer: (current, next) => {
+      return next;
+    },
+  }),
+  solution_2: new ReducedValue(z.string().default(""), {
+    reducer: (current, next) => {
+      return next;
+    },
+  }),
+  judge_recommendation: new ReducedValue(
+    z.object().default({
+      solution_1_score: 0,
+      solution_2_score: 0,
+    }),
+    {
+      reducer: (current, next) => {
+        return next;
+      },
+    },
+  ),
+});
+
+const SolutionNode: GraphNode<typeof State> = (state: typeof State) => {
+  console.log(state.messages);
 };
+
+const graph = new StateGraph(State)
+  .addNode("Solution", SolutionNode)
+  .addEdge(START, "Solution")
+  .compile();
+
+export default async function (userMessage: string) {
+  const result = await graph.invoke({
+    messages: [new HumanMessage(userMessage)],
+  });
+
+  return result.messages;
+}
